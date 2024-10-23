0a919aecba21ca0971e6330f7129751b86f36fdc
Abraxas-365 <luisfmiranda8@gmail.com>
2024-10-23 20:28:29 +0000

    chore: deliting unused functions

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -1,10 +1,5 @@
 package auth
 
-import (
-	"github.com/Abraxas-365/toolkit/pkg/lucia"
-	"github.com/gofiber/fiber/v2"
-)
-
 type AuthUser struct {
 	ID         string
 	Email      string
@@ -15,15 +10,4 @@ type AuthUser struct {
 
 func (au *AuthUser) GetID() string {
 	return au.ID
-}
-
-// Return user id and error
-func GetUserId(c *fiber.Ctx) (int, error) {
-	session := lucia.GetSession(c)
-	userId, err := session.UserIDToInt()
-	if err != nil {
-		return 0, err
-	}
-
-	return userId, nil
 }
